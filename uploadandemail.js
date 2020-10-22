905e149b51093dc0ef8f59ad20480c5ee58112f0
Zach Perkins <partofaplan@gmail.com>
2020-10-22 12:54:10 -0500

    tabbed lines 44-50

--- uploadandemail.js
+++ uploadandemail.js
@@ -41,11 +41,11 @@ http.createServer(function (req, res) {
       });
     }); 
   } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submite">');
-    res.write('</form>');
-    return res.end;
-  }
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+      res.write('<input type="file" name="filetoupload"><br>');
+      res.write('<input type="submite">');
+      res.write('</form>');
+      return res.end;
+    }
 }).listen(8080);
