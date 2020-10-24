e6adda7e9afa6207818854ebedda98de15bf7976
Zach Perkins <partofaplan@gmail.com>
2020-10-24 14:52:28 -0500

    Modified the proxy pass through the timeout

--- uploadandemail.js
+++ uploadandemail.js
@@ -12,43 +12,41 @@ var transporter = nodemailer.createTransport({
 });
 
 var srvr = http.createServer(function (req, res) {
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-	form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.path;
-      var dir = './uploads/';
-      if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir);
-      }
-      var newpath = dir + '_' + files.filetoupload.name;
-	  fs.rename(oldpath, newpath, function (err) {
-	    if (err) throw err;
-	    res.write('File uploaded');
-	    res.end();
-	  });
-	  var mailOptions = {
-        from: 'zkptest1985@gmail.com',
-        to: 'zperki5@wgu.edu',
-        subject: 'File was uploaded',
-        text: files.filetoupload.name + ' was uploaded!'
-      };
-	  transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-      } else {
-          console.log('Email sent: ' + info.response);
-      }
-      });
-    }); 
-  } else {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-      res.write('<input type="file" name="filetoupload"><br>');
-      res.write('<input type="submite">');
-      res.write('</form>');
-      return res.end;
+    if (req.url == '/fileupload') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            var oldpath = files.filetoupload.path;
+            var dir = './uploads/';
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir);
+            }
+            var newpath = dir + '_' + files.filetoupload.name;
+            fs.rename(oldpath, newpath, function (err) {
+                if (err) throw err;
+                res.write('File uploaded');
+                res.end();
+            });
+            var mailOptions = {
+                from: 'zkptest1985@gmail.com',
+                to: 'zperki5@wgu.edu',
+                subject: 'File was uploaded',
+                text: files.filetoupload.name + ' was uploaded!'
+            };
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+        });
+    } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+        res.write('<input type="file" name="filetoupload"><br>');
+        res.write('<input type="submite">');
+        res.write('</form>');
+        return res.end;
     }
-    srvr.timeout = 2000;
-    console.log(srvr.timeout);
 }).listen(9000);
 
